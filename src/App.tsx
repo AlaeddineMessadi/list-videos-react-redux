0e1c4f3267e7fc35f2cd38c576275a48ffe2a081
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-12 15:33:20 +0100

    Use Base Layout for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import { VideosTable } from './components/videos-table';
 import { getVideos } from './services/videos';
 import { ProcessedVideo } from './common/interfaces';
+import { BaseLayout } from './containers/layout';
 
 const App: React.FC = () => {
   const [videos, setVideos] = useState<ProcessedVideo[]>([]);
 
   useEffect(() => {
-    getVideos()
-      .then((videos) => {
-        setVideos(videos);
-      });
+    getVideos().then((videos) => {
+      setVideos(videos);
+    });
   }, []);
 
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Videos</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <VideosTable videos={videos} />
-      </Container>
-    </>
-  );
+  const videosList = <VideosTable videos={videos} />;
+  return <BaseLayout page={videosList} />;
 };
 
 export default App;
