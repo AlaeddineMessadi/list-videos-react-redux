53d75b04919331b9afaa62ff4524c144cf0cd900
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-12 15:27:02 +0100

    move form-control to components folder

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Container, Divider, FormControl, Grid, InputLabel, makeStyles, MenuItem, Select, TextField } from '@material-ui/core';
-
+import { MultipleSelector } from '../components/multiple-selector';
+import { FormControlElm } from '../components/form-control';
+import { SelectInputElm } from '../components/select-input';
 import { getCategories } from '../services/categories';
 import { Author, Category } from '../common/interfaces';
 import { getAuthors } from '../services/authors';
@@ -36,12 +38,64 @@ const useStyles = makeStyles((theme) => ({
 export const AddPage: React.FC = () => {
   const classes = useStyles();
 
+  const [categories, setCategories]: [Category[], (categories: Category[]) => void] = React.useState<Category[]>([]);
+  const [authors, setAuthors]: [Author[], (loading: Author[]) => void] = React.useState<Author[]>([]);
+
+  React.useEffect(() => {
+    getCategories().then((value) => {
+      setCategories(value);
+    });
+
+    getAuthors().then((value) => {
+      setAuthors(value);
+    });
+  }, []);
+
   return (
     <Container className={classes.root}>
       <Divider className={classes.divider} />
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <h1>Add Video</h1>
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.label}>
+          <label>Video Name</label>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <FormControlElm>
+            <TextField fullWidth label="Video name" variant="outlined" />
+          </FormControlElm>
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.label}>
+          <label>Video author</label>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <FormControlElm>
+            <SelectInputElm authors={authors} />
+          </FormControlElm>
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.label}>
+          <label>Video Category</label>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <FormControlElm>
+            <MultipleSelector categories={categories} label="Category" />
+          </FormControlElm>
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.label} />
+        <Grid item xs={12} sm={8}>
+          <FormControlElm>
+            <div className={classes.btnContainer}>
+              <Button variant="contained" size="medium" color="primary" className={classes.btnMargin}>
+                Submit
+              </Button>
+              <Button variant="contained" size="medium" color="secondary" className={classes.btnMargin}>
+                <Link to={`/`} className={classes.link}>
+                  Cancel
+                </Link>
+              </Button>
+            </div>
+          </FormControlElm>
         </Grid>
       </Grid>
     </Container>
