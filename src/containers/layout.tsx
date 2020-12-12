7d854b27b5ab2db6253ddb111e144d71c2373f3f
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-12 15:19:07 +0100

    Add react-router dependency + setup Routes and link in the layout

--- src/containers/layout.tsx
+++ src/containers/layout.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
 
+import { BrowserRouter as Router, Switch, Route, useRouteMatch, useParams } from 'react-router-dom';
+
+import { ListPage } from '../pages/list';
+import { AboutPage } from '../pages/about';
+import { FAQPage } from '../pages/faq';
+import { HeaderMenu } from '../components/header-menu';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -16,9 +23,17 @@ export const BaseLayout: React.FC<BaseLayoutProps> = ({ page }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <HeaderMenu />
-      <Container></Container>
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <HeaderMenu />
+        <Container>
+          <Switch>
+            <Route path="/about" component={AboutPage} />
+            <Route path="/faq" component={FAQPage} />
+            <Route path="/" component={ListPage} />
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 };
