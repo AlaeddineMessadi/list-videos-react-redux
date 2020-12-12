ccec3576570550533c930f6907665acb40f7d033
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-12 14:40:09 +0100

    Implement getVideos function in videos service

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -3,10 +3,21 @@ import { getAuthors } from './authors';
 import { ProcessedVideo } from '../common/interfaces';
 
 export const getVideos = (): Promise<ProcessedVideo[]> => {
-  return Promise
-    .all([getCategories(),getAuthors()])
-    .then(([categories, authors]) => {
-    // TODO: implement
-    return [];
+  return Promise.all([getCategories(), getAuthors()]).then(([categories, authors]) => {
+    let videos: ProcessedVideo[] = [];
+
+    authors.map((author) =>
+      author.videos.map(({ id, catIds, name }, j) => {
+        let single: ProcessedVideo = {
+          id,
+          name,
+          author: author.name,
+          categories: catIds.map((catId) => categories[categories.findIndex((v) => v.id === catId)].name),
+        };
+
+        videos.push(single);
+      })
+    );
+    return videos;
   });
 };
