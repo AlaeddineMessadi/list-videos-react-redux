19c2236beeb19e1ee302c0e45a4deb5256b30507
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-12 14:38:17 +0100

    Render VideosTable with fetched data

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid } from '@material-ui/core';
+import { VideosTable } from '../components/videos-table';
 
 import { ProcessedVideo } from '../common/interfaces';
+import { getVideos } from '../services/videos';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -24,11 +27,27 @@ const defaultVideos: ProcessedVideo[] = [];
 export const ListPage: React.FC<ListPageProps> = ({ list }) => {
   const classes = useStyles();
 
+  // Videos State
+  const [videos, setVideos]: [ProcessedVideo[], (videos: ProcessedVideo[]) => void] = React.useState(defaultVideos);
+
+  // Loading State
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    getVideos().then((value) => {
+      setVideos(value);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <Container className={classes.root}>
       <Grid container spacing={3} alignItems="flex-end">
         <Grid item xs={12}>
           <h1>List of Videos</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <VideosTable videos={videos} />
         </Grid>
       </Grid>
     </Container>
