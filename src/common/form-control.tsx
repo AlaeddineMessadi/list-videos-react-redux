d0e289b4fbc676de5c4ec001913a1d1efe6d1523
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-12 15:23:02 +0100

    Create FormControlElm component

---
+++ src/common/form-control.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Divider, FormControl, Grid, InputLabel, makeStyles, MenuItem, Select, TextField } from '@material-ui/core';
+import { MultipleSelector } from '../components/multiple-selector';
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: '100%',
+    display: 'flex',
+  },
+}));
+
+interface FormControlProps {
+  children?: JSX.Element | JSX.Element[];
+}
+
+export const FormControlElm: React.FC<FormControlProps> = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <FormControl variant="outlined" className={classes.formControl}>
+      {children}
+    </FormControl>
+  );
+};
